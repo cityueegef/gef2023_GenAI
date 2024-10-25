f6e3835d8de080ae0aacffc2ceaa2befb3cec988
abdulsanka <36437794+abdulsanka@users.noreply.github.com>
2024-10-25 11:43:49 +0800

    Delete phase 4/Lab 4.6 directory

--- phase 4/Lab 4.6/gef_lab4_6/DPU-PYNQ-3.5/pynq_dpu/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2021 Xilinx, Inc
-#
-# SPDX-License-Identifier: Apache-2.0
-
-from .dpu import DpuOverlay
-
-__version__ = '3.5.0'
--- phase 4/Lab 4.6/gef_lab4_6/prepare_docker.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-mkdir -p docker/dockerfiles/PROMPT
-wget https://github.com/Xilinx/Vitis-AI/raw/v3.5/docker_run.sh
-wget -O docker/dockerfiles/PROMPT/PROMPT_cpu.txt https://github.com/Xilinx/Vitis-AI/raw/v3.5/docker/dockerfiles/PROMPT/PROMPT_cpu.txt
-wget -O docker/dockerfiles/PROMPT/PROMPT_gpu.txt https://github.com/Xilinx/Vitis-AI/raw/v3.5/docker/dockerfiles/PROMPT/PROMPT_gpu.txt
-wget -O docker/dockerfiles/PROMPT/PROMPT_rocm.txt https://github.com/Xilinx/Vitis-AI/raw/v3.5/docker/dockerfiles/PROMPT/PROMPT_rocm.txt
-chmod u+x docker_run.sh
